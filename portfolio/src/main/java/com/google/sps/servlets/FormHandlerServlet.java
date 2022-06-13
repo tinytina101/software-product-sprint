5ce3d798794b7266a760ce622a079016bf160b48
tina liu <tl28625@uga.edu>
2022-06-13 18:16:13 -0400

    Update FormHandlerServlet.java
    can see who submitted now

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -15,11 +15,13 @@ public class FormHandlerServlet extends HttpServlet {
     // Get the value entered in the form.
     String textValue = request.getParameter("text-input");
 
+    String name = request.getParameter("name");
+
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println( name + " submitted: " + textValue);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println(name + " submitted: " + textValue);
 
     //redirect to index
     response.sendRedirect("/index.html");
